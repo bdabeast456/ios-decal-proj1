6902e067a10c11bd883314eb63ee60e4fdaa0105
Brandon Pearl <brandonpearl@berkeley.edu>
2017-02-28 20:41:33 -0800

    made font bold

--- Hangman/Hangman/HangmanViewController.swift
+++ Hangman/Hangman/HangmanViewController.swift
@@ -112,18 +112,6 @@ class HangmanViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
-    
     @IBAction func playerPushedChar(_ sender: UIButton) {
         if (hangmanGame.isRunning()) {
             if let guess = sender.titleLabel?.text {
